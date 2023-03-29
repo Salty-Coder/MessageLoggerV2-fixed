40f954e28fb30c46aa85554cf5f5b6c3fe8ace29
Daniel <davilareko@gmail.com>
2023-03-29 19:27:30 +0200

    Fix "Remove From Log" wrong color

--- Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
+++ Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name MessageLoggerV2
- * @version 2.4
+ * @version 2.5
  * @invite NYvWdN5
  * @source https://github.com/Davilarek/MessageLoggerV2-fixed/blob/master/Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Davilarek/MessageLoggerV2-fixed/master/Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
@@ -43,7 +43,7 @@ module.exports = class MessageLoggerV2 {
   }
   getVersion() {
 	// this.alreadyTestedForUpdate = false;
-    return '2.4';
+    return '2.5';
   }
   getAuthor() {
     return 'Lighty, Davilarek';
@@ -4959,7 +4959,7 @@ module.exports = class MessageLoggerV2 {
                       this.deleteEditedMessageFromRecord(messageId, parseInt(editNum));
                       this.dispatcher.dispatch({ type: 'MLV2_FORCE_UPDATE_MESSAGE_CONTENT', id: messageId });
                     },
-                    { color: 'colorDanger' }
+                    { /*color: 'colorDanger'*/ className: ZLibrary.WebpackModules.getByProps("colorDanger", "colorPremium")["colorDanger"] }
                   );
                 }
               }
@@ -4984,7 +4984,7 @@ module.exports = class MessageLoggerV2 {
                 this.dispatcher.dispatch({ type: 'MLV2_FORCE_UPDATE_MESSAGE_CONTENT', id: messageId });
               }
             },
-            { color: 'colorDanger' }
+            { /*color: 'colorDanger'*/ className: ZLibrary.WebpackModules.getByProps("colorDanger", "colorPremium")["colorDanger"] }
           );
         }
       }
@@ -5619,7 +5619,7 @@ module.exports = class MessageLoggerV2 {
                           this.dispatcher.dispatch({ type: 'MLV2_FORCE_UPDATE_MESSAGE_CONTENT', id: messageId });
                         },
                         this.obfuscatedClass('delete-edit'),
-                        { color: 'colorDanger' }
+                        { /*color: 'colorDanger'*/ className: ZLibrary.WebpackModules.getByProps("colorDanger", "colorPremium")["colorDanger"] }
                       );
                     }
                   }
@@ -5645,7 +5645,7 @@ module.exports = class MessageLoggerV2 {
                   }
                 },
                 this.obfuscatedClass('remove-from-log'),
-                { color: 'colorDanger' }
+                { /*color: 'colorDanger'*/ className: ZLibrary.WebpackModules.getByProps("colorDanger", "colorPremium")["colorDanger"] }
               );
             }
           }
